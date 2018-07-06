875890cb2920c122147b81977f571b20b9395b75
Aparna-Bindhu <aparna371997@gmail.com>
2018-07-06 20:42:05 +0530

    jquerycode

---
+++ phpques.php
@@ -0,0 +1,21 @@
+1. Write a PHP script to print the following, 
+
+•	'I'm about to learn PHP!'
+•	'You could use Zend\Symfony'    
+•	I"t's called "Product Tree""
+•	"\0 is called as a null character"
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ques</title>
+</head>
+<body>
+<?php
+ echo "'I'm about to learn PHP!'";
+ echo "'You could use Zend\Symfony'"; 
+echo "I"t's called "Product Tree"""; 
+echo "\0 is called as a null character"; 
+ 
+ ?>
+</body>
+</html>
